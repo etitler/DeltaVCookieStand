500315d666efff933e30d1898d0f477e450a3637
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-06-11 16:25:03 -0500

    used DOM to make lists

--- js/app.js
+++ js/app.js
@@ -45,32 +45,32 @@ var iowaCity3={
   },
 };
 
-var coralville={
+var coralville1={
   minCustomers: 20,
   maxCustomers: 38,
   averageSales: 2.3,
   hourlyArray:[],
   numOfCust: function(){
-    return Math.ceil(Math.random()*(coralville.maxCustomers-coralville.minCustomers)+coralville.minCustomers);
+    return Math.ceil(Math.random()*(coralville1.maxCustomers-coralville1.minCustomers)+coralville1.minCustomers);
   },
   cookiesPurchased: function(){
     for(var i=0; i<15; i++){
-      coralville.hourlyArray[i]= Math.floor(this.numOfCust()*coralville.averageSales);
+      coralville1.hourlyArray[i]= Math.floor(this.numOfCust()*coralville1.averageSales);
     }
   },
 };
 
-var northLiberty={
+var northLiberty1={
   minCustomers: 2,
   maxCustomers: 16,
   averageSales: 4.6,
   hourlyArray:[],
   numOfCust: function(){
-    return Math.ceil(Math.random()*(northLiberty.maxCustomers-northLiberty.minCustomers)+northLiberty.minCustomers);
+    return Math.ceil(Math.random()*(northLiberty1.maxCustomers-northLiberty1.minCustomers)+northLiberty1.minCustomers);
   },
   cookiesPurchased: function(){
     for(var i=0; i<15; i++){
-      northLiberty.hourlyArray[i]= Math.floor(this.numOfCust()*northLiberty.averageSales);
+      northLiberty1.hourlyArray[i]= Math.floor(this.numOfCust()*northLiberty1.averageSales);
     }
   },
 };
@@ -82,11 +82,27 @@ function simlulate() {
   iowaCity2.cookiesPurchased();
   iowaCity3.numOfCust();
   iowaCity3.cookiesPurchased();
-  coralville.numOfCust();
-  coralville.cookiesPurchased();
-  northLiberty.numOfCust();
-  northLiberty.cookiesPurchased();
+  coralville1.numOfCust();
+  coralville1.cookiesPurchased();
+  northLiberty1.numOfCust();
+  northLiberty1.cookiesPurchased();
+}
+
+var hours=["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"];
+
+function storeData(location,id){
+  var locationlist=document.getElementById(id);
+  for(var liIndex=0; liIndex<location.hourlyArray.length; liIndex++){
+    var li=document.createElement("li");
+    var listString=hours[liIndex]+": "+location.hourlyArray[liIndex]+" cookies.";
+    li.textContent=listString;
+    locationlist.appendChild(li);
+  }
 }
 
 simlulate();
-console.log(iowaCity1);
+storeData(iowaCity1,"IC1");
+storeData(iowaCity2,"IC2");
+storeData(iowaCity3,"IC3");
+storeData(coralville1,"CV1");
+storeData(northLiberty1,"NL1");
